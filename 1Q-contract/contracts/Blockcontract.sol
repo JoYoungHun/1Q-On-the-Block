c1678d9a90fc7cd4e92178a5596ed52900e86853
조영훈 <dudgns0227@gmail.com>
2023-08-16 00:49:11 +0900

    8.15 final commit

--- 1Q-contract/contracts/Blockcontract.sol
+++ 1Q-contract/contracts/Blockcontract.sol
@@ -3,26 +3,45 @@ pragma solidity 0.8.0;
 
 contract Blockcontract {
 
+    address public owner;
+
     struct BetInfo {
         uint256 curBlockNumber; // 현재 블록 넘버
         uint256 winBlockNumber; // 맞추려고 하는 정답 블록
-        address bettor; // 베터의 주소
         uint256 seed; // 베터가 지정한 시드 넘버 
         bytes32 seedtobyte; // 베터가 지정한 시드넘버를 바이트로 변환
     }
 
-    function calculateSHA256(uint256 value) internal pure returns (bytes32) {
+    mapping (uint256 => BetInfo) private _bets;
+    uint256 private _index = 0;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function calculateSHA256(uint256 value) public pure returns (bytes32) {
         return bytes32(sha256(abi.encodePacked(value)));
     }
 
-    function SaveBetInfo(uint256 _winBlockNumber, uint256 _seed) public {
+
+    // 베팅 정보 저장
+    function SaveBetInfo(uint256 _seed) public returns (bool result) {
         BetInfo memory newBet;
         newBet.curBlockNumber = block.number;
         newBet.winBlockNumber = newBet.curBlockNumber + 5;
-        newBet.bettor = msg.sender;
         newBet.seed = _seed;
         newBet.seedtobyte = calculateSHA256(_seed);
+
+        _bets[_index] = newBet;
+        _index++;
+        return true;
     }
+
+    function popInfo(uint256 index) internal returns (bool) {
+        delete _bets[index];
+        return true;
+    }
+
 
     function compareBytes(bytes32 a, bytes32 b) internal pure returns (uint256) {
         uint256 count = 0;
@@ -34,9 +53,16 @@ contract Blockcontract {
         return count;
     }
 
-    function OpenWinner(BetInfo memory newBet) public view returns (bytes32, bytes32, uint256) {
-        bytes32 answerHash = blockhash(newBet.winBlockNumber);
-        uint256 matchingDigits = compareBytes(newBet.seedtobyte, answerHash);
-        return (answerHash, newBet.seedtobyte, matchingDigits);
+    function OpenWinner() public returns (bytes32, bytes32, uint256) {
+        require(_index >= 0, "no bet."); // 추가된 부분
+
+        BetInfo memory b;
+        b = _bets[_index];
+        bytes32 answerHash = blockhash(b.winBlockNumber);
+        uint256 matchingDigits = compareBytes(b.seedtobyte, answerHash);
+        popInfo(_index);
+        _index--;
+        return (answerHash, b.seedtobyte, matchingDigits);
     }
+
 }
--- react-1q/src/App.js
+++ react-1q/src/App.js
@@ -1,87 +1,145 @@
-import React, { useState, useEffect } from 'react';
-import Web3 from 'web3';
+import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
 
-const lotteryAddress = '0x3E3159F41E9988FCb7914711eA97aa9c289e4ACD'; // 스마트 컨트랙트 주소
-const lotteryABI = [ { "inputs": [ { "internalType": "uint256", "name": "_winBlockNumber", "type": "uint256" }, { "internalType": "uint256", "name": "_seed", "type": "uint256" } ], "name": "SaveBetInfo", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "components": [ { "internalType": "uint256", "name": "curBlockNumber", "type": "uint256" }, { "internalType": "uint256", "name": "winBlockNumber", "type": "uint256" }, { "internalType": "address", "name": "bettor", "type": "address" }, { "internalType": "uint256", "name": "seed", "type": "uint256" }, { "internalType": "bytes32", "name": "seedtobyte", "type": "bytes32" } ], "internalType": "struct Blockcontract.BetInfo", "name": "newBet", "type": "tuple" } ], "name": "OpenWinner", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" }, { "internalType": "bytes32", "name": "", "type": "bytes32" }, { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function", "constant": true } ]; // 스마트 컨트랙트 ABI 배열
+import React, { useEffect, useState } from 'react';
 
+
+const lotteryAddress = '0xF7a66886D540b1884606f850Ab65e63eD550a5C3';
+const lotteryABI = [ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "owner", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [ { "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "calculateSHA256", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "pure", "type": "function", "constant": true }, { "inputs": [ { "internalType": "uint256", "name": "_seed", "type": "uint256" } ], "name": "SaveBetInfo", "outputs": [ { "internalType": "bool", "name": "result", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "OpenWinner", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" }, { "internalType": "bytes32", "name": "", "type": "bytes32" }, { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "nonpayable", "type": "function" } ];
 function App() {
   const [lotteryContract, setLotteryContract] = useState(null);
-  const [bettingInProgress, setBettingInProgress] = useState(false);
+  const [seednum, setSeednum] = useState("");
+  const [account, setAccount] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
   const [countdown, setCountdown] = useState(10);
-  const [betInfo, setBetInfo] = useState(null);
-  const [betResult, setBetResult] = useState('');
+  const [openWinnerResult, setOpenWinnerResult] = useState(null);
+
+
 
   useEffect(() => {
+    // 컴포넌트가 마운트된 후 실행되는 코드
+    // 컴포넌트가 랜더링 될 때마다 실행된다.
+
+    // initWeb3 함수 호출하여 Ethereum 관련 초기화 작업 수행
     initWeb3();
+  
+    return () => {
+      // 컴포넌트가 언마운트되거나 업데이트되기 직전에 실행되는 코드 (cleanup)
+    };
+  
+    // useEffect의 두 번째 인자로 전달되는 배열 ([])은 "의존성 배열" 또는 "의존성 리스트"라고 불림
+    // 주로 컴포넌트가 최초로 렌더링될 때 초기화 작업이나 데이터 가져오기 등을 수행할 때 사용
   }, []);
 
   async function initWeb3() {
     try {
+      // 브라우저가 메타마스크를 지원하는지 확인
       if (window.ethereum) {
+        console.log('최신 모드');
+        // 이더리움 프로바이더 설정
+        // 최신 모드에서는 window.ethereum.request를 사용하여 계정 접근권한 요청 가능
         window.web3 = new Web3(window.ethereum);
+
+        // metamask 계정 접근 권한 요청
         await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+
       } else if (window.web3) {
+        console.log('레거시 모드');
         window.web3 = new Web3(window.web3.currentProvider);
       } else {
-        console.log('Ethereum browser not detected. Try installing MetaMask.');
+        console.log('이더리움 브라우저가 아닙니다. MetaMask를 시도해보세요!');
         return;
       }
+      let contract = new window.web3.eth.Contract(lotteryABI, lotteryAddress);
 
-      setLotteryContract(new window.web3.eth.Contract(lotteryABI, lotteryAddress));
+      setLotteryContract(contract);
+
+      const account = await contract.methods.owner().call();
+      setAccount(account);
+      console.log('Owner:', account);
+
+
+
     } catch (error) {
-      console.error('Error:', error);
+      console.error('오류 발생:', error);
     }
   }
 
-  async function placeBet() {
-    setBettingInProgress(true);
-    // Call SaveBetInfo function
-    await lotteryContract.methods.SaveBetInfo(0, 0).send({ from: await getSelectedAccount() });
-    setBettingInProgress(false);
-    setCountdown(10);
+  const saveBet = async () => {
+    if (!seednum) {
+      console.log('Please enter a valid seed number.');
+      return;
+    }
 
-    // Start the countdown timer
-    const interval = setInterval(() => {
-      setCountdown(prevCountdown => prevCountdown - 1);
-    }, 1000);
+    try {
+      const nonce = await window.web3.eth.getTransactionCount(account);
+      await lotteryContract.methods.SaveBetInfo(seednum).send({
+        from: account,
+        nonce: nonce,
+      });
 
-    setTimeout(() => {
-      clearInterval(interval);
-    }, 10000);
+      console.log('Bet saved successfully!');
+
+
+      // Start the countdown and disable the button
+      setCountdown(10);
+      setIsButtonDisabled(true);
+      
+      const countdownInterval = setInterval(() => {
+        setCountdown((prevCount) => prevCount - 1);
+      }, 1000);
+
+      // After 10 seconds, reset the countdown and enable the button
+      setTimeout(() => {
+        clearInterval(countdownInterval);
+        setIsButtonDisabled(false);
+        setCountdown(10);
+      }, 10000);
+
+    } catch (error) {
+      console.error('Error saving bet:', error);
+    }
   }
 
-  async function getSelectedAccount() {
-    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-    return accounts[0];
-  }
+  const openWinner = async () => {
+    try {
+      //const currentBet = /* Create a BetInfo object based on user input */
+      const result = await lotteryContract.methods.OpenWinner().call();
 
-  async function openWinner() {
-    if (!betInfo) return;
-
-    const response = await lotteryContract.methods.OpenWinner(betInfo).call();
-    setBetResult(response);
+      // Set the OpenWinner result
+      setOpenWinnerResult(result);
+    } catch (error) {
+      console.error('Error opening winner:', error);
+    }
   }
+  
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Lottery Betting App</h1>
-        <button onClick={placeBet} disabled={bettingInProgress}>
-          {bettingInProgress ? 'Placing Bet...' : 'Place Bet'}
-        </button>
-        <p>Countdown: {countdown} seconds</p>
-        <button onClick={openWinner} disabled={!betInfo}>
-          Open Winner
-        </button>
-        {betResult && (
-          <div>
-            <p>Answer Hash: {betResult[0]}</p>
-            <p>Seedtobyte: {betResult[1]}</p>
-            <p>Matching Digits: {betResult[2]}</p>
-          </div>
-        )}
-      </header>
+      <h1>Lottery Betting App</h1>
+      <input
+        type="number"
+        value={seednum}
+        onChange={(e) => setSeednum(Number(e.target.value))}
+      />
+      <button onClick={saveBet} disabled={isButtonDisabled}>
+        {isButtonDisabled ? `Saving... (${countdown}s)` : 'Save Bet'}
+      </button>
+
+      <button onClick={openWinner}>Open Winner</button>
+
+      {/* Display the OpenWinner result */}
+      {openWinnerResult && (
+        <div>
+          <h2>Open Winner Result</h2>
+          <p>Answer Hash: {openWinnerResult[0]}</p>
+          <p>Seed to Byte: {openWinnerResult[1]}</p>
+          <p>Matching Digits: {openWinnerResult[2]}</p>
+        </div>
+      )}
     </div>
   );
 }
